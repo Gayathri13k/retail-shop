006c4a69362d03b4d36528c656b44204ba342c8e
Gayathri13k <aksumgayathri1331@gmail.com>
2025-08-26 00:47:06 +0530

    document- update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 # -----------------------
@@ -24,7 +24,7 @@ resource "aws_internet_gateway" "eks_igw" {
 resource "aws_subnet" "eks_public_subnet_1" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "eks-public-subnet-1"
@@ -34,7 +34,7 @@ resource "aws_subnet" "eks_public_subnet_1" {
 resource "aws_subnet" "eks_public_subnet_2" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "ap-south-1b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
     Name = "eks-public-subnet-2"
